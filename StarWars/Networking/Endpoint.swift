098744711ed8d16c2478ef54d0f279930644137c
cjchirag <chirag@chiragjadhwani.com>
2019-10-10 13:36:13 -0700

    4

--- StarWars/Networking/Endpoint.swift
+++ StarWars/Networking/Endpoint.swift
@@ -11,14 +11,17 @@ import Foundation
 protocol Endpoint {
     var base: String { get }
     var path: String { get }
-    var queryItems: [URLQueryItem] { get }
+    var queryItem: URLQueryItem? { get }
 }
 
 extension Endpoint {
     var urlComponents: URLComponents {
         var components = URLComponents(string: base)!
         components.path = path
-        components.queryItems = queryItems
+        
+        if let queryItem = queryItem {
+            components.queryItems = [queryItem]
+        }
         
         return components
     }
@@ -35,33 +38,24 @@ enum DataType: String {
     case Starship = "starships"
 }
 
-enum StarWars {
-    case search(name: String, type: DataType)
-    case data(type: DataType)
+enum StarWarsEndpoint: String {
+    case people
+    case starships
+    case vehicles
+    case films
 }
 
-extension StarWars: Endpoint {
+extension StarWarsEndpoint: Endpoint {
+    var queryItem: URLQueryItem? {
+        return nil
+    }
+    
     var base: String {
         return "https://swapi.co"
     }
     
     var path: String {
-        switch self {
-        case .search(_, let type): return "/api/\(type.rawValue)"
-        case .data(let type): return "/api/\(type.rawValue)"
-        }
-    }
-    
-    var queryItems: [URLQueryItem] {
-        switch self {
-        case .search(let name,_):
-            var result = [URLQueryItem]()
-            let searchName = URLQueryItem(name: "search", value: name)
-            result.append(searchName)
-            return result
-        case .data:
-            return []
-        }
+        return "/api/\(self.rawValue)"
     }
     
 }
--- StarWars/Networking/StarWarsAPI.swift
+++ StarWars/Networking/StarWarsAPI.swift
@@ -8,37 +8,82 @@
 
 import Foundation
 
-
-
-
-
-class StarWarsAPI<T: Decodable> {
-    let session: URLSession
-    
-    init(configuration: URLSessionConfiguration){
-        self.session = URLSession(configuration: configuration)
-    }
-    
-    convenience init() {
-        self.init(configuration: .default)
-    }
-    let decoder = JSONDecoder()
-    
+protocol Resource {
+    var name: String {get}
+    var category: Category {get}
+    var endpoint: StarWarsEndpoint {get}
 }
 
-private extension StarWarsAPI {
-    static func getCollection(request: URLRequest, completion: @escaping (Result<CollectionResults<T>, StarWarsError>) -> Void){
+enum Category {
+    case people
+    case vehicles
+    case starships
+}
+
+struct Collection<T: Decodable>: Decodable {
+    var count: Int
+    var next: String?
+    var previous: String?
+    var results: [T]
+    
+    private enum CodingKeys: String, CodingKey {
+        case count
+        case next
+        case previous
+        case results
+    }
+}
+
+
+
+class StarWarsAPI<T: Resource> where T: Decodable {
+    static var session: URLSession {
+        return URLSession(configuration: .default)
+    }
+    
+    static var decoder: JSONDecoder{
+        let decoder = JSONDecoder()
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "yyyy-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        
+        return decoder
+    }
+    
+    func getData(request: URLRequest, completion: @escaping (Result<CollectionResults<T>, StarWarsError>) -> Void){
+        
+    }
+    
+    
+    
+    
+    static func getCollection(request: URLRequest, completion: @escaping (Result<CollectionResults<T>, StarWarsError>) -> Void) {
         performRequestWith(request: request) { result in
-            switch result {
+            switch result{
             case .success(let data):
+                do {
                     let jsonResponse = try self.decoder.decode(CollectionResults<T>.self, from: data)
                     completion(.success(jsonResponse))
+                } catch let DecodingError.dataCorrupted(context) {
+                    print(context)
+                } catch let DecodingError.keyNotFound(key, context){
+                    print("Key: \(key) not found. There is an error")
+                    print("CodingPath: \(context.codingPath)")
+                } catch let DecodingError.valueNotFound(value, context){
+                    print("Value: \(value) not found")
+                    print("CodingPath:", context.codingPath)
+                } catch {
+                    print("Error: ", error)
+                }
             case .failure(let error):
                 completion(.failure(error))
             }
         }
     }
-    static func performRequestWith(request: URLRequest, completion: @escaping (Result<Data, StarWarsError>) -> Void) {
+    
+    
+     static func performRequestWith(request: URLRequest, completion: @escaping (Result<Data, StarWarsError>) -> Void) {
         
         
         let task = session.dataTask(with: request) { (data, response, error) in
@@ -62,3 +107,16 @@ private extension StarWarsAPI {
         task.resume()
     }
 }
+/*
+ static func getCollection(request: URLRequest, completion: @escaping (Result<CollectionResults<T>, StarWarsError>) -> Void){
+ performRequestWith(request: request) { result in
+ switch result {
+ case .success(let data):
+ let jsonResponse = try self.decoder.decode(CollectionResults<T>.self, from: data)
+ completion(.success(jsonResponse))
+ case .failure(let error):
+ completion(.failure(error))
+ }
+ }
+ }
+ */
